a620bc0116eeaedbc2be1f0faace3113d549da13
pallav98 <kumar_pallav@mckinsey.com>
2025-02-18 09:13:39 +0530

    adding backend

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,7 +1,7 @@
 resource "aws_rds_cluster" "aurora" {
   cluster_identifier      = var.cluster_name
   engine                 = "aurora-postgresql"
-  engine_version         = "15.3"
+  engine_version         = "16.6"
   database_name          = var.db_name
   master_username        = var.db_username
   master_password        = var.db_password
@@ -9,6 +9,7 @@ resource "aws_rds_cluster" "aurora" {
   serverlessv2_scaling_configuration {
     min_capacity = 0.5
     max_capacity = 2
+    seconds_until_auto_pause = 3600
   }
   vpc_security_group_ids = [var.security_group_id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
@@ -19,4 +20,12 @@ resource "aws_rds_cluster" "aurora" {
 resource "aws_db_subnet_group" "main" {
   name       = "aurora-subnet-group"
   subnet_ids = var.subnet_ids
+}
+
+
+resource "aws_rds_cluster_instance" "aurora_instances" {
+  cluster_identifier = aws_rds_cluster.aurora.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.aurora.engine
+  engine_version     = aws_rds_cluster.aurora.engine_version
 }
